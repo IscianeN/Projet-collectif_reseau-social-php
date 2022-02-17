1f7d80401d35d0c731cb1eb2299fced4982ed01a
Sarah Watremet <sarah.watremet@gmail.com>
2022-02-17 10:24:21 +0100

    Added a login button

--- src/navrefactoring.php
+++ src/navrefactoring.php
@@ -18,12 +18,17 @@
                 <!-- <a href="tags.php?tag_id=<?php echo $tag['id'] ?>">Mots-clés</a> -->
             </nav>
             <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes abonnements</a></li>
-                </ul>
+                <!-- <div id="login"> -->
+                    <a id="login" href="login.php">Login</a>
+                <!-- </div> -->
+                <div id="profil"> 
+                    <a href="#">Profil</a>
+                    <ul>
+                        <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Paramètres</a></li>
+                        <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes suiveurs</a></li>
+                        <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes abonnements</a></li>
+                    </ul>
+                </div>
 
             </nav>
         </header>
