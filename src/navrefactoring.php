67008ce222728c10e1b892ccdc0701d7ba020100
Quentin <91956889+SadeQL@users.noreply.github.com>
2022-02-17 12:21:48 +0100

    tag system works correctly

--- src/navrefactoring.php
+++ src/navrefactoring.php
@@ -4,17 +4,17 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>ReSoC - Administration</title> 
-        <meta name="author" content="Julien Falconnet">
+        <title>AAAMAzing Social Network</title> 
+        <meta name="author" content="Team A IQMS">
         <link rel="stylesheet" href="./style.css"/>
     </head>
     <body id = "body">
         <header id = "header">
             <a href="admin.php"> <img src="logo_php.png" alt="Logo de notre réseau social"/></a>
             <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mur</a>
-                <a href="feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Flux</a>
+                <a href="news.php">News</a>
+                <a href="wall.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Wall</a>
+                <a href="feed.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Feed</a>
                 <!-- <a href="tags.php?tag_id=<?php echo $tag['id'] ?>">Mots-clés</a> -->
             </nav>
             <nav id="user">
@@ -22,11 +22,11 @@
                     <a id="login" href="login.php">Login</a>
                 <!-- </div> -->
                 <div id="profil"> 
-                    <a href="#">Profil</a>
+                    <a href="#">Profile</a>
                     <ul>
-                        <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Paramètres</a></li>
-                        <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes suiveurs</a></li>
-                        <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Mes abonnements</a></li>
+                        <li><a href="settings.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Parameters</a></li>
+                        <li><a href="followers.php?user_id=<?php echo $_SESSION['connected_id'] ?>">My Followers</a></li>
+                        <li><a href="subscriptions.php?user_id=<?php echo $_SESSION['connected_id'] ?>">Followed</a></li>
                     </ul>
                 </div>
 
--- src/wall.php
+++ src/wall.php
@@ -7,36 +7,23 @@
         
         <div id="wrapper">
             <?php
-            /**
-             * Etape 1: Le mur concerne un utilisateur en particulier
-             * La première étape est donc de trouver quel est l'id de l'utilisateur
-             * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
-             * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
-             * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
-             */
+
             $userId =intval($_GET['user_id']);
 
-          
             ?>
+
             <?php
-            /**
-             * Etape 2: se connecter à la base de donnée
-             */
+   
             $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+
             ?>
 
             <aside>
-                <?php
-                /**
-                 * Etape 3: récupérer le nom de l'utilisateur
-                 */                
+                <?php          
+
                 $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId'";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $user = $lesInformations->fetch_assoc();
-            
-                
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                // echo "<pre>" . print_r($user, 5) . "</pre>";
     
                 ?>
                
@@ -65,41 +52,34 @@
                         </dl>
                         <input type='submit'>
                     </form> 
+
                 <?php
 
                 $enCoursDeTraitement = isset($_POST['message']);
+                $tagProcessing = isset($_POST['tag']);
 
-                if ($enCoursDeTraitement){
+                if ($enCoursDeTraitement && $tagProcessing){
         
                 $authorId = $userId ;
                 $postContent = $_POST['message'];
-                
                 $authorId = intval($mysqli->real_escape_string($authorId));
                 $postContent = $mysqli->real_escape_string($postContent);
-               
+                $tagContent = $_POST['tag'];
+                $tagContent = $mysqli->real_escape_string($tagContent);
+                $tagInsert =   "INSERT INTO tags "
+                . "(id, label) "
+                . "VALUES (NULL, "
+                . "'" . $tagContent . "');"
+                ;
                  $lInstructionSql = "INSERT INTO posts "
                  . "(id, user_id, content, created, parent_id) "
                  . "VALUES (NULL, "
                  . $authorId . ", "
                  . "'" . $postContent . "', "
                  . "NOW(), "
-                 /* . "'', " */
                  . "NULL);"
                  ;
 
-                 $getPostId = "
-                 SELECT $postContent, posts.id as post_id FROM posts; 
-                 SELECT tags.id as tag_id FROM tags";
-
-                 $insertPostId = "INSERT INTO posts_tags "
-                 . "(id, post_id, tag_id) "
-                 . "VALUES (NULL, "
-                 . "post_id, "
-                 . "tag_id);";
-            
-              
-                 
-                // echo $lInstructionSql;
                 $ok = $mysqli->query($lInstructionSql);
                 if ( ! $ok)
                 {
@@ -108,70 +88,78 @@
                 {
                     echo "Message posté";
                 }
+                $tagOK = $mysqli->query($tagInsert);
+                if (! $tagOK){
+                    echo "Impossible d'ajouter le tag: " . $mysqli->error;
+                } else {
+                    echo "Tag posté";
+                }
+
+                $getPostId = "SELECT posts.id as post_id FROM posts WHERE posts.content = '$postContent'; "; 
+                $getTagId = "SELECT tags.id as tag_id FROM tags WHERE tags.label = '$tagContent' ;";
                 
+                $getPostIdOk = $mysqli->query($getPostId);
+                $getTagIdOk = $mysqli->query($getTagId);
+                $postResult = $getPostIdOk->fetch_assoc();
+                $tagResult = $getTagIdOk->fetch_assoc();
              
+
+              
+                  $insertTagPostId = "INSERT INTO posts_tags "
+                 . "(id,post_id,tag_id) "
+                 . "VALUES (NULL, "
+                 . $postResult['post_id'] . ", "
+                 . $tagResult['tag_id'] . "); ";
+  
+                 $tagPostOK = $mysqli->query($insertTagPostId);
+                 if (! $tagPostOK){
+                     echo "Impossible d'ajouter dans la table: " . $mysqli->error;
+                 } else {
+                     echo "Réussi";
+                 }
+            
             }
 
          
-
-            $tagProcessing = isset($_POST['tag']);
+        /*     $tagProcessing = isset($_POST['tag']);
             if ($tagProcessing){
-                //$tagId=$_POST['tag'];
+
                 $tagContent = $_POST['tag'];
-                //$tagId = intval($mysqli->real_escape_string($tagId));
+             
                 $tagContent = $mysqli->real_escape_string($tagContent);
-               /*  echo $tagContent; */
+  
                 $tagInsert =   "INSERT INTO tags "
                 . "(id, label) "
                 . "VALUES (NULL, "
                 . "'" . $tagContent . "');"
             ;
           
-           /*  $tagInsert = "INSERT INTO tags "
-                 . "(id, label) "
-                 . "VALUES (NULL, "
-                 . $tagContent . "); "
-                 ;
-            */
-              
-
              $tagOK = $mysqli->query($tagInsert);
              if (! $tagOK){
                  echo "Impossible d'ajouter le tag: " . $mysqli->error;
              } else {
                  echo "Tag posté";
              }
-
-            }
+            } */
                 
                  //requête pour récupérer id_post 
 
-            //   $postId =intval($_GET['tag_id']);
-              $getTable = "
-              SELECT posts.id as post_id FROM posts; 
-              SELECT tags.id as tag_id FROM tags;";
-
-
-            //   $tagPostProcessing = isset($getTable);
+        /*       $getPostId = "SELECT posts.id as post_id FROM posts WHERE posts.content = '$ok'; "; 
+              $getTagId = "SELECT tags.id as tag_id FROM tags WHERE tags.label = '$tagOK' ;";
+              
             
-               $insertPostId = "INSERT INTO posts_tags "
+                $insertTagPostId = "INSERT INTO posts_tags "
                . "(id,post_id,tag_id) "
                . "VALUES(NULL, "
                . "post_id, "
                . "tag_id);";
-            
-            
-            //   $idPost = $mysqli->query($getTable);
-            //   $message = $idPost->fetch_assoc();
-      
-              $insertPostTag = $mysqli->query($insertPostId);
-              if ( ! $insertPostTag)
-              {
-                  echo("Échec de la requete : " . $mysqli->error);
-              }
-      
 
-                //  * Etape 3: récupérer tous les messages de l'utilisatrice
+               $tagPostOK = $mysqli->query($insertTagPostId);
+               if (! $insertTagPostId){
+                   echo "Impossible d'ajouter le tag: " . $mysqli->error;
+               } else {
+                   echo "Tag posté";
+               } */
                  
                 $laQuestionEnSql = "
                     SELECT posts.content, posts.created, users.alias as author_name, users.id as author_id, 
@@ -185,19 +173,16 @@
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     ";
+
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 if ( ! $lesInformations)
                 {
                     echo("Échec de la requete : " . $mysqli->error);
                 }
-        
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 */
+
                 while ($post = $lesInformations->fetch_assoc())
                 {
 
-                    // echo "<pre>" . print_r($post, 1) . "</pre>";
                     ?>   
                      
                  
@@ -207,8 +192,6 @@
                         </h3>
                         <address><a href="wall.php?user_id=<?php echo $post['author_id']?>"><?php echo $post['author_name'] ?></a></address>
                         <div>
-                            <!-- <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p> -->
                             <p><?php echo $post['content'] ?></p>
                         </div>                                            
                         <footer>
